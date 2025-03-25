11fa5a18ffdff65ebe4eaf4ba4796e238b85592a
jonasaberger <145124362+jonasaberger@users.noreply.github.com>
2025-03-25 12:17:56 +0100

    Jimmy Projekt weg - meins eini

--- 01_Demo/VaadinTestProject/src/main/java/org/vaadin/example/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.vaadin.example.repository;
-
-import org.vaadin.example.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
